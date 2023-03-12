45807d21026c7a9b1a388e08f4bd24c36627a6a4
JAXPLE <jaxple@gmail.com>
2023-03-12 20:11:43 +0900

    오목연습

---
+++ Omok/omok.js
@@ -0,0 +1,23 @@
+const mapSize = 10;
+let boardNum = 0;
+
+const root = document.querySelector(".root");
+
+setMAP();
+function setMAP(){
+    for (let i = 0; i < mapSize; i++) {
+        const firstLowBox = document.createElement("div");
+        firstLowBox.setAttribute("class","rowBox");
+        firstLowBox.textContent = boardNum++;
+    }
+
+    boardNum = 0;
+
+    for (let i = 0; i < mapSize; i++) {
+        for (let j = 0; j < mapSize; j++) {
+            const box = document.createElement("div");
+            box.setAttribute("class","box");
+            box.textContent = boardNum++;
+        }
+    }
+}
